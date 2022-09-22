9fbb01cefb2dea80f34eda2a2e60f347162caeaa
stephenzwj <stephenzwj.gmail.com>
2022-09-23 06:38:02 +0800

    fix yaml file reading

--- assets/js/common.js
+++ assets/js/common.js
@@ -8,19 +8,6 @@ $(document).ready(function() {
     $('a').removeClass('waves-effect waves-light');
 });
 
-const urls = {
-    ECE: 'https://cde.nus.edu.sg/ece/',
-    DIPTI: "https://www.ece.nus.edu.sg/gems/profhome.html",
-    GEMS: "https://www.ece.nus.edu.sg/gems/",
-    NUS: "https://www.nus.edu.sg/",
-    HUST: "http://english.hust.edu.cn/",
-    AIA: "http://english.aia.hust.edu.cn/",
-    STANFORD: "https://www.stanford.edu/",
-    GOOGLE_SCHOLAR: "https://scholar.google.com/citations?user=aanT6TIAAAAJ&hl=en&oi=ao",
-    RESEARCH_GATE: "https://www.researchgate.net/profile/Wenjie-Zhang-10",
-    THESIS: "https://scholarbank.nus.edu.sg/handle/10635/166266",
-    GRAB: "https://www.grab.com/sg/"
-  };
 // const fs = require('fs');
 // const urls = jsyaml.load(fs.readFileSync("_data/venues.yml", 'utf8'));
 // const urls2 = jsyaml.load(fs.readFileSync("_data/coauthors.yml", 'utf8'));
@@ -28,7 +15,28 @@ const urls = {
 //     urls[key] = value[0];
 //   }
 // console.log(urls);
+function readYAML( url )
+{
+     var result = null;
+     $.ajax({
+        url: url,
+        type: 'get',
+        dataType: 'html',
+        async: false,
+        success: function(data) {
+            result = jsyaml.load( data );
+        } 
+     });
+     return result;
+}
+const urls = readYAML('https://raw.githubusercontent.com/eewenjie/eewenjie.github.io/master/_data/venues.yml');
+const urls2 = readYAML('https://raw.githubusercontent.com/eewenjie/eewenjie.github.io/master/_data/coauthors.yml');
+for (const [key, value] of Object.entries(urls2)) {
+    urls[key] = value[0];
+}
+console.log(urls);
+console.log("Hello, world!");
 function showWindow(name) {
-    url = urls[name];
+    url = urls[name].url;
     window.open(url, "_blank", "directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes");
 }
